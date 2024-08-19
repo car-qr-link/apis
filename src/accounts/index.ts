6662e8bd1e8374f965ad35f19c1b65bf2eed6551
Aleksandr Soloshenko <capcom@soft-c.ru>
2024-08-19 09:11:53 +0700

    [docs] add some comments

--- src/accounts/index.ts
+++ src/accounts/index.ts
@@ -1,32 +1,91 @@
 import { BaseAccount as BaseAccount, Contact, Qr } from "../common";
 
+
+/**
+ * Interface representing an Account.
+ *
+ * This interface extends the BaseAccount interface and adds an optional
+ * 'name' field.
+ */
 export interface Account extends BaseAccount {
+    /**
+     * User name. Optional.
+     */
     name?: string;
 }
 
+
+/**
+ * Interface representing the parameters for getting an account by QR.
+ */
 export interface GetByQrParams {
+    /**
+     * The QR object used to identify the account.
+     */
     qr: Qr;
 }
 
-export interface GetContactParams {
+/**
+ * Interface representing the parameters for getting an account by contact.
+ */
+export interface GetByContactParams {
+    /**
+     * The contact object used to identify the account.
+     */
     contact: Contact;
 }
 
-export type FindAccountRequest = GetByQrParams | GetContactParams;
 
+/**
+ * Union type representing the possible request types for finding an account.
+ *
+ * This type is used to represent the different ways in which an account can be
+ * found. It can be either a `GetByQrParams` object or a `GetByContactParams`
+ * object.
+ */
+export type FindAccountRequest =
+    GetByQrParams |
+    GetByContactParams;
+
+/**
+ * Interface representing the response from a FindAccount request.
+ */
 export interface FindAccountResponse {
+    /**
+     * The account data.
+     */
     account: Account;
+
+    /**
+     * The QRs associated with the account.
+     */
     qrs: Qr[];
 }
 
+/**
+ * Interface representing the request to link a QR to an account.
+ */
 export interface LinkQRRequest {
+    /**
+     * The account data.
+     */
     account: Omit<Account, "id">;
+
+    /**
+     * The QR to link to the account.
+     */
     qr: Qr;
 }
 
 export type LinkQrResponse = FindAccountResponse;
 
+/**
+ * Interface representing the request to edit an account.
+ */
 export interface EditAccountRequest {
+    /**
+     * The account data to update.
+     */
     account: Omit<Account, "id">;
 }
 
--- src/web/index.ts
+++ src/web/index.ts
@@ -1,17 +1,41 @@
 import { Account } from "../accounts";
 import { Contact, Qr } from "../common";
-import { Notification, Reason } from "../notification";
+import { Answer, Notification, Reason } from "../notification";
 
 interface VerifyContactRequested {
     requestId: string;
 }
 
+
+/**
+ * Interface representing the payload for verifying a contact.
+ *
+ * This interface represents the payload that is sent when a user verifies a
+ * contact. It contains the requestId and the code that was sent to the user.
+ */
 interface VerifyContactPayload {
+    /**
+     * The id of the request.
+     */
     requestId: string;
+
+    /**
+     * The code that was sent to the user for verification.
+     */
     code: string;
 }
 
+
+/**
+ * Interface representing the request for logging in.
+ *
+ * This interface represents the request sent when a user wants to log in.
+ * It contains the phone number of the user.
+ */
 export interface LoginRequest {
+    /**
+     * The phone number of the user.
+     */
     phoneNumber: string;
 }
 
@@ -19,16 +43,46 @@ export type LoginResponse = VerifyContactRequested;
 
 export type VerifyLoginRequest = VerifyContactPayload;
 
+/**
+ * Interface representing the response from a login attempt.
+ *
+ * This interface represents the response that is sent back when a user
+ * attempts to log in.
+ */
 export interface VerifyLoginResponse {
+    /**
+     * The token that the user should use to authorize future requests.
+     */
     token: string;
 }
 
+/**
+ * Interface representing the response from a GetAccount request.
+ *
+ * This interface represents the response that is sent back when a user
+ * requests their account information.
+ */
 export interface GetAccountResponse {
+    /**
+     * The account of the user.
+     */
     account: Account;
+    /**
+     * The QR codes associated with the account.
+     */
     qrs: Qr[];
 }
 
+/**
+ * Interface representing the request to update a contact.
+ *
+ * This interface represents the request that is sent when a user wants to
+ * update their contact information.
+ */
 export interface UpdateContactsRequest {
+    /**
+     * The contact information to be updated.
+     */
     contact: Contact;
 }
 
@@ -38,15 +92,51 @@ export type VerifyContactRequest = VerifyContactPayload;
 
 export type VerifyContactResponse = GetAccountResponse;
 
+/**
+ * Interface representing the request to send a notification.
+ *
+ * This interface represents the request that is sent when a user wants to
+ * send a notification. It contains the id of the QR code associated with
+ * the account.
+ */
 export interface SendNotificationRequest {
+    /**
+     * The id of the QR code associated with the account.
+     */
     qr: Pick<Qr, 'id'>;
 }
 
+/**
+ * Interface representing the response from a notification request.
+ *
+ * This interface represents the response that is sent when a user requests
+ * to send a notification.
+ */
 export interface SendNotificationResponse {
-    notification: Pick<Notification, 'id'>;
+    /**
+     * The notification that was sent.
+     */
+    notification: Notification;
+    /**
+     * The answer to the notification, if any.
+     */
+    answer?: Answer;
+    /**
+     * The contact information of the account.
+     */
+    contact?: Contact;
 }
 
+/**
+ * Interface representing the request to update a notification.
+ *
+ * This interface represents the request that is sent when a user wants to
+ * update a notification.
+ */
 export interface UpdateNotificationRequest {
+    /**
+     * The id of the reason associated with the notification.
+     */
     reason: Pick<Reason, 'id'>;
 }
 
